e2fe466e395f04b7a0b794226019e78ef3d251c5
LucasBil <lucas.billy@etu.univ-savoie.fr>
2023-02-04 14:58:03 +0100

    Création de la classe des commandes du bot. Problème sur la lecture des messages

---
+++ MyProject_BotDiscord/Modules/Commands.cs
@@ -0,0 +1,25 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject_BotDiscord.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync()
+        {
+            await ReplyAsync("Pong !");
+        }
+
+        [Command("avatar")]
+        public async Task AvatarAsync(ushort size = 512)
+        {
+            await ReplyAsync(CDN.GetUserAvatarUrl(Context.User.Id, Context.User.AvatarId, size, ImageFormat.Auto));
+        }
+    }
+}
--- MyProject_BotDiscord/Program.cs
+++ MyProject_BotDiscord/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Discord;
@@ -18,7 +19,14 @@ namespace MyProject_BotDiscord
 
         public async Task RunBotAsync()
         {
-            client = new DiscordSocketClient();
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            commands = new CommandService();
+
+            client.Log += Log;
 
             client.Ready += () =>
             {
@@ -26,10 +34,46 @@ namespace MyProject_BotDiscord
                 return Task.CompletedTask;
             };
 
+            await InstallCommandsAsync();
+
             await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken",EnvironmentVariableTarget.User));
             await client.StartAsync();
 
             await Task.Delay(-1);
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage pMessage)
+        {
+            var message = pMessage as SocketUserMessage;
+            if (message == null) return;
+
+            int argPos = 0; // Definition du ! comme commande
+            if (!message.HasCharPrefix('!', ref argPos) && !message.HasMentionPrefix(client.CurrentUser, ref argPos))
+            {
+                Console.WriteLine("Pas le bon préfix");
+                return;
+            }
+
+            var context = new SocketCommandContext(client, message);
+
+            var result = await commands.ExecuteAsync(context, argPos, null);
+
+            // Si il y a une erreur dans la commande alors envoyer un message d'erreur
+            if (!result.IsSuccess) 
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.ToString());
+            return Task.CompletedTask;
         }
     }
 }
