02a12c657774e77eaea0782b2de6ea0fc54c6085
LucasBil <lucas.billy@etu.univ-savoie.fr>
2023-02-04 11:59:09 +0100

    Création de la méthode de connection du bot. Attention le token du bot est enregistrer dans une variable d'environnement

--- MyProject_BotDiscord/Program.cs
+++ MyProject_BotDiscord/Program.cs
@@ -3,13 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
 
 namespace MyProject_BotDiscord
 {
     class Program
     {
-        static void Main(string[] args)
+        private DiscordSocketClient client;
+        private CommandService commands;
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+
+        public async Task RunBotAsync()
         {
+            client = new DiscordSocketClient();
+
+            client.Ready += () =>
+            {
+                Console.WriteLine("Je suis prêt");
+                return Task.CompletedTask;
+            };
+
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken",EnvironmentVariableTarget.User));
+            await client.StartAsync();
+
+            await Task.Delay(-1);
         }
     }
 }
